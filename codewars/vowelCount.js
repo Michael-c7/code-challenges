3821a08cb9f8d60d8c779902ff19648d203ed277
Michael Carr <mcarr2468@gmail.com>
2024-04-09 12:35:53 -0500

    added another solution for vowelCount

--- codewars/vowelCount.js
+++ codewars/vowelCount.js
@@ -1,3 +1,4 @@
+// The general solution you'd expect
 function getCount(str) {
     let vowels = ["a", "e", "i", "o", "u"];
     let count = 0;
@@ -10,3 +11,30 @@ function getCount(str) {
     }
     return count;
 }
+
+
+// Solution without any built in javascript methods
+// function getCount(str) {
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+  
+//   // Iterate through each character of the input string
+//   for(let i = 0; i < str.length; i++) {
+//     let isVowel = false;
+    
+//     // Check if the current character is a vowel
+//     for (let j = 0; j < vowels.length; j++) {
+//       if (str[i] === vowels[j]) {
+//         isVowel = true;
+//         break;
+//       }
+//     }
+    
+//     // If the current character is a vowel, increase the count
+//     if (isVowel) {
+//       count++;
+//     }
+//   }
+  
+//   return count;
+// }
