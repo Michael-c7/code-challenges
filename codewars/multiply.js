733c5752fef706ccddab366016d6db8b146d2c65
Michael Carr <mcarr2468@gmail.com>
2024-04-18 13:04:41 -0500

    add multiply solution

---
+++ codewars/multiply.js
@@ -0,0 +1,12 @@
+// function declaration
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  // function expression
+  const multiply = function (a, b) {
+    return a * b;
+  }
+  
+  // arrow function
+  const multiply = (a, b) => a * b;
